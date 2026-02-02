53e27fcc9db583cb4a68c6a7d8e651935b60ff17
Politx <lautarorossi99@gmail.com>
2026-02-02 02:09:50 -0300

    Seccion de productos

---
+++ scripts/productos.js
@@ -0,0 +1,32 @@
+const productos = [
+    {
+      titulo: "Cepillos cilíndricos modulares",
+      descripcion: "Se fabrican a partir de bases inyectadas con polipropileno espumado. El sistema modular dentado proporciona un...",
+      imagen: "../images/Cepillos/CepilloCilindrico.png" 
+    },
+    {
+      titulo: "Cepillos banda lineal",
+      descripcion: "Los cepillos de banda lineal no poseen un espesor considerable en el alma del cepillo, lo que lo hace muy...",
+      imagen: "../images/Cepillos/CepilloCable.png"
+    },
+    {
+      titulo: "Cepillo cilíndrico especial",
+      descripcion: "En muchos casos no es posible adaptar el sistema modular para la construccion de cilindros, en esos casos se...",
+      imagen: "../images/Cepillos/CepilloCilindricoEspecial.png"
+    },
+    {
+        titulo: "Perfil L",
+        descripcion: "Este tipo de cepillos se utiliza como barrera o cortina contra polvo, liquidos, insectos, etc. Es un perfil extruido con una...",
+        imagen: "../images/Cepillos/CepilloCilindrico.png" 
+      },
+      {
+        titulo: "Cepillos lineales especiales",
+        descripcion: "Los cepillos lineales se utilizan para tipo de aplicaciones e incluyen tanto los cepillos de banda lineal como los cepillos...",
+        imagen: "../images/Cepillos/CepilloCable.png" 
+      },
+      {
+        titulo: "Cepillos retorcidos",
+        descripcion: "Estos incluyen todos los cepillos fabricados a partir del alambre retorcido como baquetas, pinceles mecánicos, limpia...",
+        imagen: "../images/Cepillos/CepilloCilindricoEspecial.png" 
+      },
+  ];
