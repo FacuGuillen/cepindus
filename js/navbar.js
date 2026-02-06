c96dfbdae6aecdc698855a3c05d774f6767f46d6
FacuGuillen <facu.guillen@hotmail.com>
2026-02-06 16:58:03 -0300

    agregar seccion de contacto y mapa de ubicacion

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,12 @@
-// Usamos una ruta que suba un nivel si es necesario o 
-// simplemente asegurate de cómo la llamás.
-fetch('/components/navbar.html') // El "/" inicial intenta ir a la raíz del dominio
-    .then(response => response.text())
-    .then(data => {
-        const placeholder = document.getElementById('navbar-placeholder');
-        if (placeholder) {
-            placeholder.innerHTML = data;
-        }
-    })
-    .catch(error => console.error('Error cargando el navbar:', error));
+const btnMenu = document.getElementById("btn-menu");
+const menu = document.querySelector(".mega-menu");
+
+btnMenu.addEventListener("click", (e) => {
+    e.stopPropagation();
+    menu.classList.toggle("active"); 
+});
+
+// Cierra el menú si haces clic afuera
+window.addEventListener("click", () => {
+    menu.classList.remove("active");
+});
