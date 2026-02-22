06c7018b53b4c034b082cc8c012f70ac32cd4d3d
FacuGuillen <facu.guillen@hotmail.com>
2026-02-21 21:37:19 -0300

    Enviar mail en el formulario

--- js/navbar.js
+++ js/navbar.js
@@ -2,8 +2,17 @@ const btnMenu = document.getElementById("btn-menu");
 const menu = document.querySelector(".mega-menu");
 
 btnMenu.addEventListener("click", (e) => {
+    e.preventDefault(); // Evita que el enlace intente saltar antes de tiempo
     e.stopPropagation();
-    menu.classList.toggle("active"); 
+
+    // Verificamos si el menú ya está abierto
+    if (menu.classList.contains("active")) {
+        // Si está abierto, lo redirigimos a la página general de cepillos
+        window.location.href = "/pages/productos.html";
+    } else {
+        // Si está cerrado, solo lo abrimos
+        menu.classList.toggle("active");
+    }
 });
 
 // Cierra el menú si haces clic afuera
